26e6adb9694bcbffff97bd1eaf350c2971ef08a2
daniel-lannister <daniel_lannister@hotmail.com>
2023-12-13 08:49:19 -0300

    atualizações

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
 import "./Auth.css";
 
 // Components
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Message from "../../components/Message.jsx";
 
 // Hooks
-import { useState, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 // Redux
-import { register, reset } from '../../slices/authSlice';
+import { register, reset } from "../../slices/authSlice";
 
 const Register = () => {
   const [name, setName] = useState("");
@@ -19,32 +19,66 @@ const Register = () => {
 
   const dispatch = useDispatch();
 
-  const { loading, error } = 
+  const { loading, error } = useSelector((state) => state.auth);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const user = {
       name,
       email,
       password,
-      confirmPassword
-    }
+      confirmPassword,
+    };
+
     console.log(user);
-  }
+
+    dispatch(register(user));
+  };
+
+  // Clean all auth states
+  useEffect(() => {
+    dispatch(reset());
+  }, [dispatch]);
+
   return (
     <div id="register">
-      <h2>Register</h2>
-      <p className='subtitle'>Cadastre-se para ver as fotos dos seus amigos!</p>
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
       <form onSubmit={handleSubmit}>
-        <input type='text' placeholder='Nome' onChange={(e)=>setName(e.target.value)} value={name || ""}/>
-        <input type='email' placeholder='Email' onChange={(e)=>setEmail(e.target.value)} value={email || ""}/>
-        <input type='password' placeholder='Senha' onChange={(e)=>setPassword(e.target.value)} value={password || ""}/>
-        <input type='password' placeholder='Confirme a senha' onChange={(e)=>setConfirmPassword(e.target.value)} value={confirmPassword || ""} />
-        <input type='submit' value="Cadastrar" />
+        <input
+          type="text"
+          placeholder="Nome"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword}
+        />
+        {!loading && <input type="submit" value="Cadastrar" />}
+        {loading && <input type="submit" disabled value="Aguarde..." />}
+        {error && <Message msg={error} type="error" />}
       </form>
-      <p>Já possui conta? </p> <Link to='/login'>Clique aqui!</Link>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui</Link>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
