9b7899207bd61e44091394a4654c5781d082edb4
daniel-lannister <daniel_lannister@hotmail.com>
2023-12-14 10:26:56 -0300

    logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,68 @@
-import { useState } from 'react';
-import './App.css';
+import "./App.css";
 
-// Router
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+// Hooks
+import { useAuth } from "./hooks/useAuth";
 
-// Pages
-import Home from './pages/Home/Home';
-import Login from './pages/Auth/Login';
-import Register from './pages/Auth/Register';
+// router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-// Componentes
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+// components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
+// pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import EditProfile from "./pages/EditProfile/EditProfile";
+import Profile from "./pages/Profile/Profile";
+import Photo from "./pages/Photo/Photo";
+import Search from "./pages/Search/Search";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { auth, loading } = useAuth();
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
   return (
-    <>
-      <div>
-        <BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
         <Navbar />
-        <div className='container'>
+        <div className="container">
           <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/login' element={<Login />}/>
-            <Route path='/register' element={<Register />}/>
+            <Route
+              path="/"
+              element={auth ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/profile"
+              element={auth ? <EditProfile /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/users/:id"
+              element={auth ? <Profile /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/search"
+              element={auth ? <Search /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="login"
+              element={!auth ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="register"
+              element={!auth ? <Register /> : <Navigate to="/" />}
+            />
+            <Route path="photos/:id" element={<Photo />} />
           </Routes>
         </div>
         <Footer />
-        </BrowserRouter>
-      </div>
-    </>
-  )
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,38 +1,99 @@
-import React from 'react';
-import styles from './Navbar.module.css';
+import "./Navbar.css";
 
 // Components
-import { NavLink, Link } from 'react-router-dom';
-import { BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill } from 'react-icons/bs';
- 
+import { NavLink, Link } from "react-router-dom";
+import {
+  BsSearch,
+  BsHouseDoorFill,
+  BsFillPersonFill,
+  BsFillCameraFill,
+} from "react-icons/bs";
+
+// Hooks
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+// Redux
+import { logout, reset } from "../slices/authSlice";
 
 const Navbar = () => {
-  return (
-    <nav className={styles.nav}>
-        <Link to='/'>ReactGram</Link>
-        <form className={styles.searchform}>
-            <BsSearch/>
-            <input type='text' placeholder='Pesquisar'/>
-        </form>
-        <ul className={styles.navlinks}>
-            <li>
-                <NavLink to='/'>
-                    <BsHouseDoorFill />
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/login'>
-                    Entrar
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/register'>
-                    Cadastrar
-                </NavLink>
-            </li>
-        </ul>
-    </nav>
-  )
-}
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
 
-export default Navbar
+  const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const [query, setQuery] = useState("");
+
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+
+    navigate("/login");
+  };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
+  };
+
+  return (
+    <nav id="nav">
+      <Link to="/">
+        <h2>ReactGram</h2>
+      </Link>
+      <form id="search-form" onSubmit={handleSearch}>
+        <BsSearch />
+        <input
+          type="text"
+          placeholder="Pesquisar"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </form>
+      <ul id="nav-links">
+        {auth ? (
+          <>
+            <li>
+              <NavLink to="/">
+                <BsHouseDoorFill />
+              </NavLink>
+            </li>
+            {user && (
+              <li>
+                <NavLink to={`/users/${user._id}`}>
+                  <BsFillCameraFill />
+                </NavLink>
+              </li>
+            )}
+            <li>
+              <NavLink to="/profile">
+                <BsFillPersonFill />
+              </NavLink>
+            </li>
+            <li>
+              <span onClick={handleLogout}>Sair</span>
+            </li>
+          </>
+        ) : (
+          <>
+            {" "}
+            <li>
+              <NavLink to="/login">Entrar</NavLink>
+            </li>
+            <li>
+              <NavLink to="/register">Cadastrar</NavLink>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
